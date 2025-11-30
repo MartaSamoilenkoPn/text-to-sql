0142cae159197042c4d7031f4485ed9bf52d7bdf
MartaSamoilenkoPn <116800440+MartaSamoilenkoPn@users.noreply.github.com>
2025-11-30 23:33:12 +0200

    Update generation.py

--- generation.py
+++ generation.py
@@ -13,7 +13,7 @@ except ImportError:
     exit(1)
 
 DB_PATH = "sample.db"
-TEMPLATE_PATH = "templates/select_info.yaml"
+TEMPLATE_PATH = "select_info.yaml"
 EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 
 
